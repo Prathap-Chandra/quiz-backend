d7639c21832fc5967ed16150b45c09a376ffbed5
Prathap Chandra <prathap@Prathaps-MacBook-Air.local>
2022-04-26 20:02:23 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 6000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	return res.status(200).json({
+		success: true,
+		status: 'OK'
+	})
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
