b8374b40d596bfa366402a7940c64e07ed915ce7
Prathap Chandra <prathap@Prathaps-MacBook-Air.local>
2022-04-26 20:19:22 +0530

    added port

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = 8642;
+const PORT = process.env.PORT || 8642;
 
 app.use(express.json());
 
 app.get('/home', (req, res) => {
 	return res.status(200).json({
+		route: '/home',
+		success: true,
+		status: 'OK'
+	})
+});
+
+app.get('/home', (req, res) => {
+	return res.status(200).json({
+		route: '/',
 		success: true,
 		status: 'OK'
 	})
